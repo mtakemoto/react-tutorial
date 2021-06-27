922eb36fd98ee733aa0a0fe8e427f518233ce98a
Matt Takemoto <matt.t.takemoto@gmail.com>
2021-06-27 14:08:22 -0700

    finished completing the game section

--- learn-react-02/finish/src/index.js
+++ learn-react-02/finish/src/index.js
@@ -36,6 +36,11 @@ class Board extends React.Component {
     //Copy the array
     const squares = this.state.squares.slice();
 
+    //If won or same square, return
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     //Modify the copied object
     squares[i] = this.state.xIsNext ? 'X' : 'O';
 
@@ -56,7 +61,11 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    status = winner
+      ? `Winner: ${winner}`
+      : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
 
     return (
       <div>
@@ -103,3 +112,25 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i=0; i<lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares [a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
