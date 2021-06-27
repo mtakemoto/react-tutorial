ff17b02323ad51a4f198f342beadbb90aa848042
Matt Takemoto <matt.t.takemoto@gmail.com>
2021-06-27 14:44:53 -0700

    history completed

--- learn-react-03/finish/src/index.js
+++ learn-react-03/finish/src/index.js
@@ -71,7 +71,7 @@ class Game extends React.Component {
 
   handleClick(i) {
     //Copy the array
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
 
@@ -89,6 +89,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   }
@@ -102,13 +103,13 @@ class Game extends React.Component {
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
+        `Go to move # ${move}`:
+        `Go to game start`;
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
